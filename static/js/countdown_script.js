f809fab6bf65a2ceadd1f26bc2d36e9dac364789
Tyler Folkman <tylerfolkman@gmail.com>
2017-11-19 20:56:48 -0700

    new post

--- static/js/countdown_script.js
+++ static/js/countdown_script.js
@@ -1,6 +1,6 @@
-$('#clock').countdown('2018/08/02', function(event) {
+$('#clock').countdown('2018/07/17', function(event) {
   $(this).html(event.strftime('%D days till return'));
 });
-$('#clock2').countdown('2018/08/02', function(event) {
+$('#clock2').countdown('2018/07/17', function(event) {
   $(this).html(event.strftime('%Dd'));
 });
